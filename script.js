d60f6e938eb3e9abe0d9a0c1f5aca88f6f8fb0e2
CoderStellaJ <40347063+CoderStellaJ@users.noreply.github.com>
2019-06-08 17:58:59 +0800

    adjustments

--- script.js
+++
@@ -1,7 +0,0 @@
-
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    });
-});
